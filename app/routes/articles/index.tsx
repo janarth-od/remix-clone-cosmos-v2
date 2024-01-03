af0435ebd83297671c178e457f0fe439a426695c
janarth-od <janarth.punniyamoorthy@opendoor.com>
2024-01-03 11:49:56 -0500

    fixing up ts errors

--- app/routes/articles/index.tsx
+++ app/routes/articles/index.tsx
@@ -1,13 +1,11 @@
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { json } from '@remix-run/node';
+import type { LinksFunction } from '@remix-run/node';
+import { Link, useLoaderData } from '@remix-run/react';
+import { ContentTypeCollection } from 'contentful';
 
-import type { LinksFunction } from "@remix-run/node"; // or cloudflare/deno
+import styles from '../../styles/articles.css';
 
-import styles from "../../styles/articles.css";
-
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: styles },
-];
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
 type Article = {
   slug: string;
@@ -21,51 +19,51 @@ async function getArticles(): Promise<Article[]> {
       headers: {
         Authorization: `Bearer ${process.env.CONTENTFUL_MANAGEMENT_API_TOKEN}`,
       },
-    }
-  )
-  const res = await fetch(request)
-  const articles = await res.json()
-  console.log({articles})
+    },
+  );
+  const res = await fetch(request);
+  const articlesCollection: ContentTypeCollection = await res.json();
+  console.log({ articlesCollection });
 
-  return articles.items.slice(0,5).map ((article) => ({
+  // ignore ts errors. I didn't copy the article fields type from contentful
+  return articlesCollection.items.slice(0, 5).map((article) => ({
     slug: article.fields.slug['en-US'],
     title: article.fields.articleName['en-US'],
-  }))
+  }));
 }
 
+// Server-side loader function
+// https://remix.run/docs/en/main/route/loader#loader
+/*
+  This function is only ever run on the server. On the initial server render, it will provide data to the HTML document. 
+  On navigations in the browser, Remix will call the function via fetch from the browser.
+
+  This means you can talk directly to your database, use server-only API secrets, etc. Any code that isn't used 
+  to render the UI will be removed from the browser bundle.
+*/
 export const loader = async () => {
   return json({
     articles: await getArticles(),
   });
 };
 
-const ArticleCard = ({article}) => (
-  <div 
-    key={article.slug}
-    className="article-card"
-  >
-    <Link to={`/articles/${article.slug}`}>
-      {article.title}
-    </Link>
-  </div> 
-)
+const ArticleCard = ({ article }: {article: Article}) => (
+  <div className="article-card">
+    <Link to={`${article.slug}`}>{article.title}</Link>
+  </div>
+);
 
 export default function Articles() {
   const { articles } = useLoaderData<typeof loader>();
 
   return (
     <main>
-      <h1>
-        Articles fetched from Contentful on server side!
-      </h1>
+      <h1>Articles fetched from Contentful on server side!</h1>
       <div>
         {articles.map((article) => (
-          <ArticleCard
-            key={article.slug}
-            article={article}
-          />
+          <ArticleCard key={article.slug} article={article} />
         ))}
       </div>
     </main>
-  )
+  );
 }
